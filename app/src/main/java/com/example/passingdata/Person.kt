ca47d5c4bde243c0634462b4676fafa59345badc
Rachana <rachana.bmagar@gmail.com>
2022-08-12 11:01:19 +0545

    Finished Project.

---
+++ app/src/main/java/com/example/passingdata/Person.kt
@@ -0,0 +1,9 @@
+package com.example.passingdata
+
+import java.io.Serializable
+
+data class Person(
+    val name: String,
+    val age: Int,
+    val country: String
+): Serializable
---
+++ app/src/main/java/com/example/passingdata/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.passingdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val person = intent.getSerializableExtra("EXTRA_PERSON") as Person
+        tvPerson.text = person.toString()
+    }
+}
